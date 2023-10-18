96eaa0c185bc897278d7b5f68bd631ea6c4d242e
Victordng <89790931+Victordng86@users.noreply.github.com>
2023-10-18 18:13:31 +0200

    amended src directory

--- src/main/java/com/javatechie/aws/cicd/example/OderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OderServiceApplication.java
@@ -3,14 +3,10 @@ package com.javatechie.aws.cicd.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
+
 
 @SpringBootApplication
 @RestController
